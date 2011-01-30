284f9109b7a5e3e72d3158007ff48f934130fdd2
codester <elad.hemar@gmail.com>
2011-01-31 00:43:16 +0200

    master

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coolapp::Application.config.secret_token = 'c6dbe20003a7ef54ad35d57c3e276868dee07a4893f06e46cdbb3005a5486148a8f7abb6b222c6c5d6a6fb2c36fb4f62300e582cb0d7f703b3ba9e2e82a80e33'
